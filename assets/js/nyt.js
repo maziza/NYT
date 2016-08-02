d24e5377d19cc755def72dfc1acbce8f04206a2f
Dad <Dad>
2016-08-01 20:55:28 -0400

    added html to test jquery file and the base ajax search

---
+++ assets/js/nyt.js
@@ -0,0 +1,17 @@
+/*
+	Article Search API: 74c86f1c937b422eb4d6cda12de80cd7
+*/
+
+// Built by LucyBot. www.lucybot.com
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
+var apiKey = "74c86f1c937b422eb4d6cda12de80cd7"
+
+
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
